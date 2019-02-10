bb81a76db87a8288707b4bf96ddd5d13ae8a4d23
toshikish <toshiki@nanshika.com>
2019-02-10 23:49:59 +0900

    Add 1_1_B (Greatest Common Divisor)

---
+++ ALDS1_1_B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int x, int y) {
+    if (x < y) {
+        swap(x, y);
+    }
+    if (y == 0) return x;
+    return gcd(y, x % y);
+}
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    cout << gcd(x, y) << endl;
+
+    return 0;
+}
